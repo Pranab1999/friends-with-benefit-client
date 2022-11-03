bb6f377ec98bb204199b788080594a7dfcc29a23
Pranab Amatya <PranabAmatya@arn.com.au>
2022-11-03 13:02:16 +1100

    Account Styling

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -79,86 +79,93 @@ const Account = () => {
     };
 
     return (
-        <div className="container mx-auto main_container">
-            <div className="wrapper">
-                <div className="page_title">
+        <div className="container">
+            <div className="wrapper flex flex-col space-y-2">
+                <div className="page_title text-2xl font-bold text-left">
                     <h2>My Profile</h2>
                 </div>
-                <div className="page_options">
-                    <button onClick={updateDetails}>
+                <div className="page_options flex flex-row space-x-1">
+                    <button
+                        className='login_btn button hover:opacity-75 text-white border-2 bg-btn-green p-1 rounded w-1/4'
+                        onClick={updateDetails}>
                         Update Details
                     </button>
-                    <button onClick={updatePassword}>
+                    <button
+                        className='login_btn button hover:opacity-75 text-white border-2 bg-btn-blue p-1 rounded w-1/4'
+                        onClick={updatePassword}>
                         Change Password
                     </button>
-                    <button onClick={updateEmail}>
+                    <button
+                        className='login_btn button hover:opacity-75 text-white border-2 bg-btn-cyan p-1 rounded w-1/4'
+                        onClick={updateEmail}>
                         Change Email Address
                     </button>
-                    <button onClick={logout}>
+                    <button
+                        className='login_btn button hover:opacity-75 text-white border-2 bg-btn-red p-1 rounded w-1/4'
+                        onClick={logout}>
                         Log out
                     </button>
                 </div>
                 <div className="user_details_container">
-                    <h2>Here are your account details</h2>
                     <div className="inner_content">
-                        <ul className="user_details">
-                            <li className="detail user_tag" style={{display: 'flex'}}>
+                        <ul className="user_details text-left">
+                            <li className="detail user_tag text-left">
                                 <h5>Tag Number</h5>
                                 <p>{tag}</p>
                             </li>
-                            <li className="detail user_first_name" style={{display: 'flex'}}>
+                            <li className="detail user_first_name">
                                 <h5>First Name</h5>
                                 <p>{firstName}</p>
                             </li>
-                            <li className="detail user_last_name" style={{display: 'flex'}}>
+                            <li className="detail user_last_name">
                                 <h5>Last Name</h5>
                                 <p>{lastName}</p>
                             </li>
-                            <li className="detail user_phone" style={{display: 'flex'}}>
+                            <li className="detail user_phone">
                                 <h5>Phone</h5>
                                 <p>{phone}</p>
                             </li>
-                            <li className="detail user_email" style={{display: 'flex'}}>
+                            <li className="detail user_email">
                                 <h5>Email</h5>
                                 <p>{email}</p>
                             </li>
-                            <li className="detail user_street" style={{display: 'flex'}}>
+                            <li className="detail user_street">
                                 <h5>Street Address</h5>
                                 <p>{street}</p>
                             </li>
-                            <li className="detail user_suburb" style={{display: 'flex'}}>
+                            <li className="detail user_suburb">
                                 <h5>Town/Suburb</h5>
                                 <p>{suburb}</p>
                             </li>
-                            <li className="detail user_post_code" style={{display: 'flex'}}>
+                            <li className="detail user_post_code">
                                 <h5>PostCode</h5>
                                 <p>{postcode}</p>
                             </li>
-                            <li className="detail user_gender" style={{display: 'flex'}}>
+                            <li className="detail user_gender">
                                 <h5>Gender</h5>
                                 <p>{gender}</p>
                             </li>
-                            <li className="detail user_age" style={{display: 'flex'}}>
+                            <li className="detail user_age">
                                 <h5>Age</h5>
                                 <p>{age}</p>
                             </li>
-                            <li className="detail user_listen" style={{display: 'flex'}}>
+                            <li className="detail user_listen">
                                 <h5>How do you listen to Sea FM?</h5>
                                 <p>{listen.join(", ")}</p>
                             </li>
-                            <li className="detail user_tag_origin" style={{display: 'flex'}}>
+                            <li className="detail user_tag_origin">
                                 <h5>Tag Origin</h5>
                                 <p>{tagSource}</p>
                             </li>
-                            <li className="detail user_more_music" style={{display: 'flex'}}>
+                            <li className="detail user_more_music">
                                 <h5>Music would you like hear more of</h5>
                                 <p>{moreMusic}</p>
                             </li>
-                            <li className="detail user_offers" style={{display: 'flex'}}>
+                            <li className="detail user_offers">
                                 <h5>Receive special offers from select Sea FM advertisers</h5>
                                 <p>{offers ? 'Yes' : 'No'}</p>
                             </li>
-                            <li className="detail user_subscription" style={{display: 'flex'}}>
+                            <li className="detail user_subscription">
                                 <h5>Receive Sea FM Friends With Benefits newsletter</h5>
                                 <p>{newsletterSubscription ? 'Yes' : 'No'}</p>
                             </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
       colors: {
         'btn-green': '#21ba45',
         'btn-blue': '#2185d0',
+        'btn-cyan': '#00b5ad',
+        'btn-red': '#db2828',
         'light' : '#7a8188',
       },
     },
